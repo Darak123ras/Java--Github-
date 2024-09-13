a8e2922794171667c9192067d30611fcb175d9d4
Darakhshan <darakhshanmrashid109@gmail.com>
2024-09-13 20:03:53 +0530

    BinaryTree

---
+++ BinaryMirrorTree.java
@@ -0,0 +1,62 @@
+/*
+ * Given a Binary Tree, convert it into its mirror.
+MirrorTree1            
+
+Examples:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 3 1 2
+Explanation: The tree is
+   1    (mirror)  1
+ /  \    =>      /  \
+2    3          3    2
+The inorder of mirror is 3 1 2
+Input:
+      10
+     /  \
+    20   30
+   /  \
+  40  60
+Output: 30 10 60 20 40
+Explanation: The tree is
+      10               10
+    /    \  (mirror) /    \
+   20    30    =>   30    20
+  /  \                   /   \
+ 40  60                 60   40
+The inroder traversal of mirror is
+30 10 60 20 40.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+1 ≤ Data of a node ≤ 105
+ */
+// PayTM, Morgan Stanley, VMWare, Amazon, Accolite, Samsung, Microsoft, Snapdeal, Goldman Sachs, Walmart, SAP Labs, Google, Adobe, Myntra, Ebay
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+}
+public class BinaryMirrorTree {
+    void mirror(Node node) {
+        // Your code here
+        if(node==null) return;
+        
+        mirror(node.right);
+        mirror(node.left);
+        
+        Node temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+    }
+}
