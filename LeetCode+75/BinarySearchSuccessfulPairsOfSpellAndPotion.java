68edc57050e037bfcf4e93cf630279e296edee95
Darakhshan <darakhshanmrashid109@gmail.com>
2024-10-01 19:41:22 +0530

    Binary+Search

---
+++ LeetCode+75/BinarySearchSuccessfulPairsOfSpellAndPotion.java
@@ -0,0 +1,81 @@
+/*
+ * You are given two positive integer arrays spells and potions, of length n and m respectively, where spells[i] represents the strength of the ith spell and potions[j] represents the strength of the jth potion.
+
+You are also given an integer success. A spell and potion pair is considered successful if the product of their strengths is at least success.
+
+Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
+
+ 
+
+Example 1:
+
+Input: spells = [5,1,3], potions = [1,2,3,4,5], success = 7
+Output: [4,0,3]
+Explanation:
+- 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+- 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+- 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
+Thus, [4,0,3] is returned.
+Example 2:
+
+Input: spells = [3,1,2], potions = [8,5,8], success = 16
+Output: [2,0,2]
+Explanation:
+- 0th spell: 3 * [8,5,8] = [24,15,24]. 2 pairs are successful.
+- 1st spell: 1 * [8,5,8] = [8,5,8]. 0 pairs are successful. 
+- 2nd spell: 2 * [8,5,8] = [16,10,16]. 2 pairs are successful. 
+Thus, [2,0,2] is returned.
+ 
+
+Constraints:
+
+n == spells.length
+m == potions.length
+1 <= n, m <= 105
+1 <= spells[i], potions[i] <= 105
+1 <= success <= 1010
+ */
+
+public class BinarySearchSuccessfulPairsOfSpellAndPotion {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n=spells.length;
+        int m=potions.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++){
+            long minPotion = (success + spells[i] - 1) / spells[i];
+            int idx = binarySearch(potions, minPotion);
+            result[i] = m - idx;
+
+        }
+        return result;
+        // Approach is CORRECT but the Time Complexity is an issue, it will give O(n*m)
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     List<Integer> helper=new ArrayList<>();
+        //     int count=0;
+        //     for(int j=0;j<m;j++){
+        //         helper.add(spells[i]*potions[j]);
+        //     }
+        //     for(int j=0;j<helper.size();j++){
+        //         if(helper.get(j)>=success){
+        //             count++;
+        //         }
+        //     }
+        //     arr.add(count);
+        // }
+        // return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+    private int binarySearch(int[] potions, long target) {
+        int low = 0, high = potions.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (potions[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low; 
+    }
+}
